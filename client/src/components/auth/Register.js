d9e7d2a8c6c27e65d7d0e365fb54923538b34460
= <hire@carloshuizar.com>
2019-07-19 08:15:56 -0400

    Register user and get token back

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,15 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import AlertContext from '../../context/alert/alertContext';
+import AuthContext from '../../context/auth/authContext';
 
 const Register = () => {
   const alertContext = useContext(AlertContext);
 
   const { setAlert } = alertContext;
+
+  const authContext = useContext(AuthContext);
+
+  const { register, error, clearErrors } = authContext;
 
   const [user, setUser] = useState({
     name: '',
@@ -12,6 +17,13 @@ const Register = () => {
     password: '',
     confirmPass: ''
   });
+
+  useEffect(() => {
+    if (error === 'User already exists') {
+      setAlert(error, 'danger');
+      clearErrors();
+    }
+  }, [error]);
 
   const { name, email, password, confirmPass } = user;
 
@@ -26,7 +38,7 @@ const Register = () => {
     } else if (password !== confirmPass) {
       setAlert('Passwords do not match', 'danger');
     } else {
-      console.log('Regsiter submit');
+      register({ name, email, password });
     }
   };
 
