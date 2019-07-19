5632801a2a36747986b14644d471f9f63d641466
= <hire@carloshuizar.com>
2019-07-18 22:40:27 -0400

    filter contacts based on email, phone, name, type

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -39,6 +39,24 @@ export default (state, action) => {
           contact.id === action.payload.id ? action.payload : contact
         )
       };
+    case FILTER_CONTACT:
+      return {
+        ...state,
+        filtered: state.contacts.filter(contact => {
+          const regex = new RegExp(`${action.payload}`, 'gi');
+          return (
+            contact.name.match(regex) ||
+            contact.email.match(regex) ||
+            contact.phone.match(regex) ||
+            contact.type.match(regex)
+          );
+        })
+      };
+    case CLEAR_FILTER:
+      return {
+        ...state,
+        filtered: null
+      };
     default:
       return state;
   }
