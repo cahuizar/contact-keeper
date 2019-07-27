6ca82233346f659f70fdf04fd7f0a2b6fb955523
= <hire@carloshuizar.com>
2019-07-27 09:41:15 -0400

    add contacts to database

--- routes/contacts.js
+++ routes/contacts.js
@@ -50,7 +50,8 @@ router.post(
         type
       });
       const contact = await newContact.save();
-      res.json({ contact });
+      console.log(contact.email);
+      res.json(contact);
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
