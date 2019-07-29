f35ef98118d483790436037e2e10926f61e13f78
= <hire@carloshuizar.com>
2019-07-29 08:55:13 -0400

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -11,14 +12,22 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) =>
-  res.json({ msg: 'Welcome to the contact keeper api' })
-);
-
 // Define Routes
 app.use(`${API_URL}/users`, require('./routes/users'));
 app.use(`${API_URL}/auth`, require('./routes/auth'));
 app.use(`${API_URL}/contacts`, require('./routes/contacts'));
+
+// Server static assests in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get(
+    '*',
+    (req,
+    res =>
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
